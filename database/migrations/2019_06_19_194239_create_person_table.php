5d50b2f62c62610c6afce9abc30697e3984481ed
Manan Jadhav <curosmj@gmail.com>
2019-06-19 15:43:10 -0400

    initialise properly

---
+++ database/migrations/2019_06_19_194239_create_person_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person', function (Blueprint $table) {
+            $table->increments('person_id');
+            $table->string('person_fname');
+            $table->string('person_lname');
+            $table->unsignedInteger('mobile_no');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person');
+    }
+}
